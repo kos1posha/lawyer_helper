d640b40f08142065893f21d6bfa80f517857022c
kos1posha <alnaur-gloov@mail.ru>
2023-09-24 13:39:23 +0300

    Add logging

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-import datetime
-import sys
-import traceback
+import os, sys, traceback, logging
 
+from PySide6 import QtWidgets
 from PySide6.QtWidgets import QApplication, QMainWindow
 
 from controls.main_control import MainControl
 
+
+def excepthook(exc_type, exc_value, exc_tb):
+    tb = ''.join(traceback.format_exception(exc_type, exc_value, exc_tb))
+    logging.error(tb)
+    print(tb)
+    QtWidgets.QApplication.quit()
+
+
 if __name__ == '__main__':
-    try:
-        app = QApplication(sys.argv)
-        app.setStyle('Fusion')
-        window = QMainWindow()
-        control = MainControl(window)
-        window.show()
-        sys.exit(app.exec())
-    except Exception:
-        tb = traceback.format_exc()
-        print(tb)
-        with open('debug.log', 'a') as log:
-            log.write(f'[{datetime.datetime.now().strftime("%X %x")}]\n{tb}\n')
+    sys.excepthook = excepthook
+    logging.basicConfig(level=logging.ERROR, filename=os.path.abspath('debug.log'), filemode='a', format='[%(asctime)s]\n%(message)s', datefmt='%X %x')
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion')
+    window = QMainWindow()
+    control = MainControl(window)
+    window.show()
+    sys.exit(app.exec())
